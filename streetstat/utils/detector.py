bfcdd75a528b4c88a6fde9c180669c5926a0b7bf
WikiGenius <muhammedelyamani92@gmail.com>
2023-04-02 14:03:20 +0200

    Add detector.py for tflite

---
+++ streetstat/utils/detector.py
@@ -0,0 +1,76 @@
+# Author: Muhammed Elyamani
+# Date: 03/02/2023
+# GitHub: https://github.com/WikiGenius
+
+from threading import Thread
+from utils import draw_traffic, get_model, preprocess
+import time
+import cv2
+import numpy as np
+
+class Detector:
+    def __init__(self, model_tflite_path, use_cuda=False):
+        self.yolo_v8 = 'v8' in model_tflite_path
+        
+        self.use_cuda = use_cuda
+        self.interpreter, self.input_details, self.output_details = get_model(
+            model_tflite_path)
+        if self.yolo_v8:
+            self.input_shape = self.input_details[0]['shape'][1:3]
+        else:
+            self.input_shape = self.input_details[0]['shape'][2:]
+            
+        self.output_data = []
+        # Variable to control when model is stopped
+        self.stopped = True
+        self.detect_started = False
+        self.thread_detect = False
+        
+    def start(self):
+        # Start the thread that update_detection
+        self.thread_detect = True
+        Thread(target=self.update_detection, args=(), daemon=True).start()
+        return self
+
+    def stop(self):
+        # Indicate that the camera and thread should be stopped
+        self.stopped = True
+        
+    def update_detection(self):
+        while True:
+            if not self.stopped:
+                self.detection_process()
+
+                    
+    def detection_process(self):
+
+        transpose=False
+        if not self.yolo_v8:
+            transpose = True
+        im, self.ratio, self.dwdh = preprocess(self.img, self.input_shape, transpose)
+        
+        # predict the model
+        input = self.input_details[0]
+        self.interpreter.set_tensor(input['index'], im)
+        self.interpreter.invoke()
+        self.output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+        if self.yolo_v8:
+            self.output_data if isinstance(self.output_data, np.ndarray) else self.output_data.numpy()
+            self.output_data = np.squeeze(self.output_data[0])
+        
+        self.detect_started = True
+            
+    def detect(self, img, conf_thres=0.25, iou_thres=0.45):
+        self.img = img.copy()
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+        self.stopped = False
+        if not self.thread_detect:
+            self.detection_process()
+        else:
+            time.sleep(0.05)
+            pass
+
+
+        return self.output_data
+
