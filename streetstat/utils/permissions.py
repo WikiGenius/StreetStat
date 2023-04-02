dfaceba77df03f70717a1e523c3bbbf850f99e64
WikiGenius <muhammedelyamani92@gmail.com>
2023-04-02 13:50:15 +0200

    Add init permisions for android

---
+++ streetstat/utils/permissions.py
@@ -0,0 +1,18 @@
+from kivy import platform
+from kivy.core.window import Window
+
+# This code block is checking if the platform running the program is Android,
+# and if so, it requests read and 
+# write permissions to the external storage of the device using the android.
+# permissions module. This is necessary because on Android, 
+# apps need to request permissions to access sensitive data 
+# such as external storage.
+
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+    request_permissions([
+        Permission.WRITE_EXTERNAL_STORAGE,
+        Permission.READ_EXTERNAL_STORAGE,
+        Permission.INTERNET,
+        Permission.READ_MEDIA_VIDEO
+    ])
