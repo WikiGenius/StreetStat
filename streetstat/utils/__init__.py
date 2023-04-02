15f730bce9908b1fe75ae19eb2579d5e04b48a00
WikiGenius <muhammedelyamani92@gmail.com>
2023-04-02 13:42:53 +0200

    update init

--- streetstat/utils/__init__.py
+++ streetstat/utils/__init__.py
@@ -9,7 +9,20 @@ from utils.style_app import StyleApp
 from utils.preprocess_face import preprocess_face
 from utils.process_image import Process
 from utils.generate_report import generate_xlsx
+from utils.detector import Detector
+from utils.model import get_model
+
 import random
 random.seed(0)
 
 
+#Name of the classes according to class indices.
+NAMES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+        'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+        'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+        'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+        'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+        'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+        'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+        'hair drier', 'toothbrush']
