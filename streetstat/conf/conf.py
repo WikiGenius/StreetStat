15b04e727ced8a6f25070307c4eb30c6aecb1f6b
WikiGenius <muhammedelyamani92@gmail.com>
2023-04-02 13:48:47 +0200

    update conf

--- streetstat/conf/conf.py
+++ streetstat/conf/conf.py
@@ -2,15 +2,34 @@
 # Date: 03/02/2023
 # GitHub: https://github.com/WikiGenius
 
-import cv2
-import asone
-from ultralytics import YOLO
-import numpy as np
-detector = asone.ASOne(detector=asone.YOLOV8N_PYTORCH ,use_cuda=True)
+import os
+from kivy import platform
 
+###########################<PLATFORM SETTINGS>###############################
+
+if platform == "android":
+    PLATFORM_ANDROID = True
+else:
+    PLATFORM_ANDROID = False
+    
+if not PLATFORM_ANDROID:
+    import asone
+    detector = asone.ASOne(detector=asone.YOLOV8N_PYTORCH ,use_cuda=True)
+else:
+    from utils import Detector
+    THREAD = False
+    # model_path = '../assets/weights/yolov6n_model.tflite'
+    model_path = '../assets/weights/yolov8n_float16.tflite'
+
+    print(f"Is the model existed: {os.path.isfile(model_path)}")
+    if THREAD:
+        detector = Detector(model_path).start()
+    else:
+        detector = Detector(model_path)
+    
+###########################<GLOBAL SETTINGS>###############################
+    
 DEBUG=False
-
-
 BAR_OPACITY = 0.8
 MAX_FRAMES = 6
 SKIP_FRAMES = 5
